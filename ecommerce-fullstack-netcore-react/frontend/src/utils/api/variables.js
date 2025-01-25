b11002993aed21242b17930d3cb964bcd3ca3398
thangepe <23113822+thangtcm@users.noreply.github.com>
2025-01-25 23:43:09 +0700

    Fix

--- ecommerce-fullstack-netcore-react/frontend/src/utils/api/variables.js
+++ ecommerce-fullstack-netcore-react/frontend/src/utils/api/variables.js
@@ -1,8 +1,8 @@
 export const variables = {
-    BASE_URL: "http://demokhoaanh-001-site1.qtempurl.com/api/",
-    USER_API: "http://demokhoaanh-001-site1.qtempurl.com/api/user",
-    PRODUCT_API: "http://demokhoaanh-001-site1.qtempurl.com/api/product",
-    ORDER_API: "http://demokhoaanh-001-site1.qtempurl.com/api/order",
-    PRODUCTSIZE_API: "http://demokhoaanh-001-site1.qtempurl.com/api/productsize",
-    ORDERITEM_API: "http://demokhoaanh-001-site1.qtempurl.com/api/orderitem",
+    BASE_URL: "https://demokhoaanh-001-site1.qtempurl.com/api/",
+    USER_API: "https://demokhoaanh-001-site1.qtempurl.com/api/user",
+    PRODUCT_API: "https://demokhoaanh-001-site1.qtempurl.com/api/product",
+    ORDER_API: "https://demokhoaanh-001-site1.qtempurl.com/api/order",
+    PRODUCTSIZE_API: "https://demokhoaanh-001-site1.qtempurl.com/api/productsize",
+    ORDERITEM_API: "https://demokhoaanh-001-site1.qtempurl.com/api/orderitem",
 }
