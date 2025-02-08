9a2942e20f5583d48e550445dd769d1a81513c01
thangepe <23113822+thangtcm@users.noreply.github.com>
2025-02-08 21:51:05 +0700

    fix

--- ecommerce-fullstack-netcore-react/frontend/src/utils/api/apiClient.js
+++ ecommerce-fullstack-netcore-react/frontend/src/utils/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { variables } from './variables.js';
 
 const apiClient = axios.create({
-  baseURL: variables.ORDER_API,  
+  baseURL: variables.BASE_URL,  
   headers: {
     'Content-Type': 'application/json',  
   }
